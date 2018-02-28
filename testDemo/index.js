1986caa37edc77323e30d71d4f76e9a5f879c43a
Iverson <7681539@qq.com>
2018-02-28 11:56:11 +0800

    提交

---
+++ testDemo/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
+import App from './container/App';
+
+ReactDOM.render((
+  <Router history={browserHistory}>
+    <Route path="/" component={App} />
+  </Router>
+), document.getElementById('root'));
+
+
+ReactDOM.render((
+  <div>aaaaaaaaaa</div>
+), document.getElementById('leftmenu'));
